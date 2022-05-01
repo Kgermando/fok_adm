64bac1fdbb9fbaed9bb88f5aafa06ce66bb003b9
Germain Kataku <katakugermain@gmail.com>
2022-05-01 11:41:48 +0100

    profile done

--- lib/src/pages/auth/profil_page.dart
+++ lib/src/pages/auth/profil_page.dart
@@ -1,7 +1,11 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:fokad_admin/src/api/auth/auth_api.dart';
+import 'package:fokad_admin/src/constants/app_theme.dart';
 import 'package:fokad_admin/src/constants/responsive.dart';
 import 'package:fokad_admin/src/models/users/user_model.dart';
+import 'package:fokad_admin/src/navigation/drawer/drawer_menu.dart';
+import 'package:fokad_admin/src/widgets/title_widget.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 
@@ -13,269 +17,266 @@ class ProfilPage extends StatefulWidget {
 }
 
 class _ProfilPageState extends State<ProfilPage> {
+  final GlobalKey<ScaffoldState> _key = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          header(),
-          const SizedBox(
-            height: 20.0,
+        key: _key,
+        drawer: const DrawerMenu(),
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (Responsive.isDesktop(context))
+                const Expanded(
+                  child: DrawerMenu(),
+                ),
+              Expanded(
+                flex: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(p10),
+                  child: FutureBuilder<UserModel>(
+                    future: AuthApi().getUserId(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        UserModel? userInfo = snapshot.data;
+                        if (userInfo != null) {
+                          var userData = userInfo;
+                          return Expanded(child: profileBody(userData));
+                        }
+                      }
+                      return Container();
+                    },
+                  ),
+                ),
+              ),
+            ],
           ),
-          Expanded(
-            child: FutureBuilder<UserModel>(
-              future: AuthApi().getUserInfo(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  UserModel? userInfo = snapshot.data;
-                  if (userInfo != null) {
-                    var userData = userInfo;
-                    return SingleChildScrollView(
-                      padding: const EdgeInsets.only(bottom: 40.0),
-                      child: SizedBox(
-                        width: double.infinity,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            header(),
-                            const SizedBox(
-                              height: 20.0,
-                            ),
-                            profileBody(userData),
-                          ],
-                        ),
-                      ),
-                    );
-                  }
-                }
-                return Container();
-              },
-            ),
-          ),
-        ],
-      ),
-    );
+        ));
   }
-
-  Widget header() {
-    final headline3 = Theme.of(context).textTheme.headline3;
-    final headline5 = Theme.of(context).textTheme.headline5;
-    return SizedBox(
-      width: double.infinity,
-      child: Card(
-        child: Responsive.isDesktop(context)
-            ? Text('Mon Profil', style: headline3)
-            : Text('Mon Profil', style: headline5),
-      ),
-    );
-  }
-
 
   Widget profileBody(UserModel userModel) {
     final headline5 = Theme.of(context).textTheme.headline5;
-    final bodyText1 = Theme.of(context).textTheme.bodyText1;
-    final bodyText2 = Theme.of(context).textTheme.bodyText2;
-    return Responsive.isDesktop(context)
-      ? ListBody(
+    final bodyLarge = Theme.of(context).textTheme.bodyLarge;
+    
+    final String firstLettter2 = userModel.prenom[0];
+    final String firstLettter = userModel.nom[0];
+
+    return ListView(
+      children: [
+        Stack(
+          clipBehavior: Clip.none, 
           children: [
-            if (userModel.photo!.isNotEmpty) Image.network(userModel.photo!),
-            // SizedBox(
-            //   height: 100,
-            //   width: 100,
-            //   child: CachedNetworkImage(
-            //     imageUrl: userData.logo!,
-            //     placeholder: (context, url) => const CircularProgressIndicator(),
-            //     errorWidget: (context, url, error) => const Icon(Icons.error),
-            //   ),
-            // ),
-            const SizedBox(
-              height: 20.0,
+            
+            Container(
+              color: Colors.amber.shade700,
+              height: 200,
+              width: double.infinity,
             ),
-            Card(
-              child: ListTile(
-                leading: const Icon(Icons.person),
-                title: Text('Nom',
-                    style: GoogleFonts.poppins(textStyle: headline5)),
-                trailing: Text(userModel.nom,
-                    overflow: TextOverflow.clip,
-                    style: GoogleFonts.poppins(
-                        textStyle: headline5, fontWeight: FontWeight.bold)),
-              ),
+            Positioned(
+              top: 20,
+              left: 20,
+              child: AutoSizeText("Mon Profil", 
+                maxLines: 1,
+              style: headline5!.copyWith(color: Colors.white),)
             ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Card(
-              child: ListTile(
-                leading: const Icon(Icons.person),
-                title: Text('Prénom',
-                    style: GoogleFonts.poppins(textStyle: headline5)),
-                trailing: Text(userModel.prenom,
-                    overflow: TextOverflow.ellipsis,
-                    style: GoogleFonts.poppins(
-                        textStyle: headline5, fontWeight: FontWeight.bold)),
-              ),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Card(
-                child: ListTile(
-                  leading: const Icon(Icons.play_arrow_rounded),
-                  title: Text('Matricule',
-                      style: GoogleFonts.poppins(textStyle: headline5)),
-                  trailing: Text(userModel.matricule,
-                      overflow: TextOverflow.ellipsis,
-                      style: GoogleFonts.poppins(
-                          textStyle: headline5, fontWeight: FontWeight.bold)),
+            Positioned(
+              top: 130,
+              left: 50,
+              child: SizedBox(
+                width: 100,
+                height: 100,
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: AutoSizeText(
+                    '$firstLettter2$firstLettter'.toUpperCase(),
+                    maxLines: 1,
+                    style: headline5.copyWith(color: Colors.orange.shade700),
+                  ),
                 ),
               ),
-              const SizedBox(
-                height: 20.0,
-              ),
-            Card(
-              child: ListTile(
-                leading: const Icon(Icons.play_arrow_rounded),
-                title: Text('Niveau d\'accréditation',
-                    style: GoogleFonts.poppins(textStyle: headline5)),
-                trailing: Text(userModel.role,
-                    overflow: TextOverflow.ellipsis,
-                    style: GoogleFonts.poppins(
-                        textStyle: headline5, fontWeight: FontWeight.bold)),
-              ),
             ),
-            const SizedBox(
-              height: 20.0,
+            Positioned(
+              top: 150,
+              left: 180,
+              child: AutoSizeText(
+                "${userModel.prenom} ${userModel.nom}",
+                maxLines: 2,
+                style: headline5.copyWith(color: Colors.white)
+              )
             ),
-            Card(
-              child: ListTile(
-                leading: const Icon(Icons.access_time_filled_sharp),
-                title: Text('Date de création',
-                    style: GoogleFonts.poppins(textStyle: headline5)),
-                trailing: Text(
-                    DateFormat("dd.MM.yy HH:mm").format(userModel.createdAt),
-                    overflow: TextOverflow.ellipsis,
-                    style: GoogleFonts.poppins(
-                        textStyle: headline5, fontWeight: FontWeight.bold)),
-              ),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
+
+            Positioned(
+              top: 155,
+              right: 20,
+              child: Container(
+                width: 15,
+                height: 15,
+                decoration: BoxDecoration(
+                  color: Colors.green.shade700,
+                  shape: BoxShape.circle,
+                ),
+              )
+            )
+
           ],
-        )
-      : Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if(userModel.photo!.isNotEmpty)
-            Image.network(userModel.photo!),
-            // CachedNetworkImage(
-            //   imageUrl: userModel.logo!,
-            //   placeholder: (context, url) => const CircularProgressIndicator(),
-            //   errorWidget: (context, url, error) => const Icon(Icons.error),
-            // ),
-            const SizedBox(
-              height: 16.0,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Nom', style: bodyText2),
-                      Text(userModel.nom,
-                          overflow: TextOverflow.clip, style: bodyText1),
-                    ],
-                  ),
+        ),
+        const SizedBox(height: p50),
+        Card(
+          elevation: 10,
+          child: Padding(
+            padding: const EdgeInsets.all(p20),
+            child: Row(
+              children: [
+                Expanded(
+                  child: AutoSizeText('Nom', maxLines: 1, 
+                  style: bodyLarge!.copyWith(fontWeight: FontWeight.bold))
                 ),
-              ),
+                const SizedBox(width: p20),
+                Expanded(child: AutoSizeText(userModel.nom, maxLines: 1, style: bodyLarge))
+              ],
             ),
-            const SizedBox(
-              height: 16.0,
+          ),
+        ),
+        Card(
+           elevation: 10,
+          child: Padding(
+            padding: const EdgeInsets.all(p20),
+            child: Row(
+              children: [
+                Expanded(
+                    child: AutoSizeText('Prénom', maxLines: 1, 
+                    style: bodyLarge.copyWith(fontWeight: FontWeight.bold))),
+                const SizedBox(width: p20),
+                Expanded(
+                    child:
+                        AutoSizeText(userModel.prenom, maxLines: 1, style: bodyLarge))
+              ],
             ),
-            SizedBox(
-              width: double.infinity,
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Prénom', style: bodyText2),
-                      Text(userModel.prenom,
-                          overflow: TextOverflow.clip, style: bodyText1),
-                    ],
-                  ),
-                ),
-              ),
+          ),
+        ),
+        Card(
+         elevation: 10,
+          child: Padding(
+            padding: const EdgeInsets.all(p20),
+            child: Row(
+              children: [
+                Expanded(
+                    child: AutoSizeText('Matricule', maxLines: 1, 
+                    style: bodyLarge.copyWith(fontWeight: FontWeight.bold))),
+                const SizedBox(width: p20),
+                Expanded(
+                    child: AutoSizeText(userModel.matricule,
+                        maxLines: 1, style: bodyLarge))
+              ],
             ),
-            const SizedBox(
-                height: 16.0,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Matricule', style: bodyText2),
-                        Text(userModel.matricule,
-                            overflow: TextOverflow.clip, style: bodyText1),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 16.0,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Niveau d\'accréditation', style: bodyText2),
-                        Text(userModel.role,
-                            overflow: TextOverflow.clip, style: bodyText1),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            const SizedBox(
-              height: 16.0,
+          ),
+        ),
+        Card(
+          elevation: 10,
+          child: Padding(
+            padding: const EdgeInsets.all(p20),
+            child: Row(
+              children: [
+                Expanded(
+                    child: AutoSizeText('Département',
+                        maxLines: 1, style: bodyLarge.copyWith(fontWeight: FontWeight.bold))),
+                const SizedBox(width: p20),
+                Expanded(
+                    child: AutoSizeText(userModel.departement,
+                        maxLines: 1, style: bodyLarge))
+              ],
             ),
-            SizedBox(
-              width: double.infinity,
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Date de création', style: bodyText2),
-                      Text(
-                          DateFormat("dd.MM.yy HH:mm")
-                              .format(userModel.createdAt),
-                          overflow: TextOverflow.clip,
-                          style: bodyText1),
-                    ],
-                  ),
-                ),
-              ),
+          ),
+        ),
+        Card(
+          elevation: 10,
+          child: Padding(
+            padding: const EdgeInsets.all(p20),
+            child: Row(
+              children: [
+                Expanded(
+                    child: AutoSizeText('Services d\'Affectation',
+                        maxLines: 1, style: bodyLarge.copyWith(fontWeight: FontWeight.bold))),
+                const SizedBox(width: p20),
+                Expanded(
+                    child: AutoSizeText(userModel.servicesAffectation,
+                        maxLines: 1, style: bodyLarge))
+              ],
             ),
-            const SizedBox(
-              height: 20.0,
+          ),
+        ),
+        Card(
+          elevation: 10,
+          child: Padding(
+            padding: const EdgeInsets.all(p20),
+            child: Row(
+              children: [
+                Expanded(
+                    child: AutoSizeText('Fonction Occupée',
+                        maxLines: 1, style: bodyLarge.copyWith(fontWeight: FontWeight.bold))),
+                const SizedBox(width: p20),
+                Expanded(
+                    child: AutoSizeText(userModel.fonctionOccupe,
+                        maxLines: 1, style: bodyLarge))
+              ],
             ),
-          ],
-        );
+          ),
+        ),
+        Card(
+          elevation: 10,
+          child: Padding(
+            padding: const EdgeInsets.all(p20),
+            child: Row(
+              children: [
+                Expanded(
+                    child: AutoSizeText('Accréditation',
+                        maxLines: 1, style: bodyLarge.copyWith(fontWeight: FontWeight.bold))),
+                const SizedBox(width: p20),
+                Expanded(
+                    child: AutoSizeText(userModel.role,
+                        maxLines: 1, style: bodyLarge))
+              ],
+            ),
+          ),
+        ),
+        Card(
+          elevation: 10,
+          child: Padding(
+            padding: const EdgeInsets.all(p20),
+            child: Row(
+              children: [
+                Expanded(
+                    child: AutoSizeText('Succursale',
+                        maxLines: 1, style: bodyLarge.copyWith(fontWeight: FontWeight.bold))),
+                const SizedBox(width: p20),
+                Expanded(
+                    child: AutoSizeText(userModel.succursale,
+                        maxLines: 1, style: bodyLarge))
+              ],
+            ),
+          ),
+        ),
+        Card(
+          elevation: 10,
+          child: Padding(
+            padding: const EdgeInsets.all(p20),
+            child: Row(
+              children: [
+                Expanded(
+                    child: AutoSizeText('Date de création',
+                        maxLines: 1, style: bodyLarge.copyWith(fontWeight: FontWeight.bold))),
+                const SizedBox(width: p20),
+                Expanded(
+                    child: AutoSizeText(
+                        DateFormat("dd.MM.yy HH:mm").format(userModel.createdAt),
+                        maxLines: 1, style: bodyLarge))
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
--- lib/src/utils/menu_options.dart
+++ lib/src/utils/menu_options.dart
@@ -3,6 +3,7 @@ import 'package:fokad_admin/src/api/auth/auth_api.dart';
 import 'package:fokad_admin/src/models/menu_item.dart';
 import 'package:fokad_admin/src/routes/routes.dart';
 import 'package:fokad_admin/src/utils/menu_items.dart';
+import 'package:routemaster/routemaster.dart';
 
 class MenuOptions with ChangeNotifier {
   PopupMenuItem<MenuItem> buildItem(MenuItem item) => PopupMenuItem(
@@ -18,21 +19,21 @@ class MenuOptions with ChangeNotifier {
   void onSelected(BuildContext context, MenuItem item) {
     switch (item) {
       case MenuItems.itemProfile:
-        Navigator.of(context).pushReplacementNamed(UserRoutes.profile);
+        Routemaster.of(context).replace(UserRoutes.profile);
         break;
 
       case MenuItems.itemHelp:
-       Navigator.of(context).pushReplacementNamed(UserRoutes.helps);
+        Routemaster.of(context).replace(UserRoutes.helps);
         break;
 
       case MenuItems.itemSettings:
-        Navigator.of(context).pushReplacementNamed(UserRoutes.settings);
+        Routemaster.of(context).replace(UserRoutes.settings);
         break;
 
       case MenuItems.itemLogout:
         // Remove stockage jwt here.
         AuthApi().logout();
-        Navigator.of(context).pushReplacementNamed(UserRoutes.login);
+        Routemaster.of(context).replace(UserRoutes.login);
     }
   }
 }
